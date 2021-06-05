f2077d82f4439dcd25c520eaaf7f193b03be5520
lumen <lumenize@ajou.ac.kr>
2021-06-05 10:03:15 +0900

    SQL문 의 type 변경 _text->varchar

--- src/TeamProject/DB_Connect.java
+++ src/TeamProject/DB_Connect.java
@@ -23,30 +23,35 @@ public class DB_Connect {
 
             //DB에 저장된 테이블이 있다면 지우고 없다면 넘어가기
             String DropTable = "DROP TABLE IF EXISTS UserInfo CASCADE; DROP TABLE IF EXISTS PolicyInfo CASCADE; DROP TABLE IF EXISTS CountInfo CASCADE;";
-			//nowState.executeUpdate(DropTable);
-
+			nowState.execute(DropTable);
             //DB 생성
 
             //empmSttsCn : 취업상태 , accrRqisCn : 학력 , majrRqisCn : 전공 , splzRlmRqisCn : 특화분야
             //bizId : 정책 번호 ,polyBizSjnm : 정책명, polyItcnCn : 정책소개, plcyTpNm : 정책유형, rqutPrdCn : 신청기간 , rqutUrla : 주소, polyBizTy : 기관 및 지자채 구분, AgeInfo : 연령, empmSttsCn : 취업상태, accrRqisCn : 학력,  majrRqisCn : 전공, splzRlmRqisCn : 특화분야
             //bizId : 정책 번호, cnt : 검색횟수
 
-            String CreateUserInfo = "Create Table UserInfo(userID text(),userPassword text(), area text(), ageInfo int, empmSttsCn text(),accrRqisCn text(), majrRqisCn text(), splzRlmRqisCn text())";
-            String CreatePolicyInfo = "Create Table PolicyInfo(bizId text(),polyBizSjnm text(),polyItcnCn text(),plcyTpNm text(),rqutPrdCn text(),rqutUrla text(),polyBizTy text(),AgeInfo text(),empmSttsCn text(), accrRqisCn text(), majrRqisCn text(), splzRlmRqisCn text())";
-            String CreateCountInfo = "Create Table CountInfo(bizld text(),cnt int);";
+            String CreateUserInfo = "Create Table UserInfo(userID int,userPassword int, area varchar(20), ageInfo int, empmSttsCn varchar(20),accrRqisCn varchar(20), majrRqisCn varchar(20), splzRlmRqisCn varchar(20));";
+            String CreatePolicyInfo = "Create Table PolicyInfo(bizId varchar(100),polyBizSjnm varchar(100),polyItcnCn varchar(100),plcyTpNm varchar(200),rqutPrdCn varchar(100),rqutUrla varchar(100),polyBizTy varchar(100),AgeInfo varchar(100),empmSttsCn varchar(100), accrRqisCn varchar(100), majrRqisCn varchar(300), splzRlmRqisCn varchar(200));";
+            String CreateCountInfo = "Create Table CountInfo(bizld varchar(20),cnt int);";
 
-            //nowState.executeUpdate(CreateUserInfo);
-            //nowState.executeUpdate(CreatePolicyInfo);
-            //nowState.executeUpdate(CreateCountInfo);
-
+            nowState.execute(CreateUserInfo);
+            nowState.execute(CreatePolicyInfo);
+            nowState.execute(CreateCountInfo);
 
             String[][] Data = DB_Split.main();
 
             for(int i = 1; i < 201; i++) {
                 System.out.println("\n정책 " + i + "\n");
+                String insertSQL = "insert into PolicyInfo values(";
                 for(int j = 0; j < 12; j++) {
-                    System.out.println(Data[i][j]);
+                    System.out.println(Data[i][j].length());
+                    insertSQL += "'"+ Data[i][j]+"',";
                 }
+                insertSQL = insertSQL.replaceFirst(".$","");
+                insertSQL += ");";
+                System.out.println(insertSQL);
+                nowState.execute(insertSQL);
+                System.out.println("insert "+i);
                 System.out.println("\n------------------------------------------");
             }
 
