b4e8c01bbb8f54d11eeb74a6e44a89b609e61640
lumen <lumenize@ajou.ac.kr>
2021-06-05 10:37:30 +0900

    Policyinfo의 Create table의 인자 배치 변경

--- src/TeamProject/DB_Connect.java
+++ src/TeamProject/DB_Connect.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Random;
 import java.util.Scanner;
 
 public class DB_Connect {
@@ -27,11 +28,11 @@ public class DB_Connect {
             //DB 생성
 
             //empmSttsCn : 취업상태 , accrRqisCn : 학력 , majrRqisCn : 전공 , splzRlmRqisCn : 특화분야
-            //bizId : 정책 번호 ,polyBizSjnm : 정책명, polyItcnCn : 정책소개, plcyTpNm : 정책유형, rqutPrdCn : 신청기간 , rqutUrla : 주소, polyBizTy : 기관 및 지자채 구분, AgeInfo : 연령, empmSttsCn : 취업상태, accrRqisCn : 학력,  majrRqisCn : 전공, splzRlmRqisCn : 특화분야
+            //bizId : 정책 번호 ,polyBizSjnm : 정책명, polyItcnCn : 정책소개, plcyTpNm : 정책유형, rqutPrdCn : 신청기간 , rqutUrla : 사이트 주소, polyBizTy : 기관 및 지자채 구분, AgeInfo : 연령, empmSttsCn : 취업상태, accrRqisCn : 학력,  majrRqisCn : 전공, splzRlmRqisCn : 특화분야
             //bizId : 정책 번호, cnt : 검색횟수
 
             String CreateUserInfo = "Create Table UserInfo(userID int,userPassword int, area varchar(20), ageInfo int, empmSttsCn varchar(20),accrRqisCn varchar(20), majrRqisCn varchar(20), splzRlmRqisCn varchar(20));";
-            String CreatePolicyInfo = "Create Table PolicyInfo(bizId varchar(100),polyBizSjnm varchar(100),polyItcnCn varchar(100),plcyTpNm varchar(200),rqutPrdCn varchar(100),rqutUrla varchar(100),polyBizTy varchar(100),AgeInfo varchar(100),empmSttsCn varchar(100), accrRqisCn varchar(100), majrRqisCn varchar(300), splzRlmRqisCn varchar(200));";
+            String CreatePolicyInfo = "Create Table PolicyInfo(bizId varchar(100),polyBizTy varchar(100),polyBizSjnm varchar(100),polyItcnCn varchar(200),plcyTpNm varchar(100),ageInfo varchar(100),empmSttsCn varchar(100),accrRqisCn varchar(100),majrRqisCn varchar(100), splzRlmRqisCn varchar(100), rqutPrdCn varchar(300), rqutUrla varchar(200));";
             String CreateCountInfo = "Create Table CountInfo(bizld varchar(20),cnt int);";
 
             nowState.execute(CreateUserInfo);
@@ -39,6 +40,9 @@ public class DB_Connect {
             nowState.execute(CreateCountInfo);
 
             String[][] Data = DB_Split.main();
+
+
+
 
             for(int i = 1; i < 201; i++) {
                 System.out.println("\n정책 " + i + "\n");
@@ -55,6 +59,15 @@ public class DB_Connect {
                 System.out.println("\n------------------------------------------");
             }
 
+            for(int i = 1; i < 201; i++) {
+                Random random = new Random();
+                System.out.println("\n유저 " + i + "\n");
+                String insertSQL_UserInfo = "insert into UserInfo values('"+i+"','"+i+"','서울_차후 수정','"+(random.nextInt(60)+20)+"','"+Data[random.nextInt(200)][6]+"','"+Data[random.nextInt(200)][7]+"','"+Data[random.nextInt(200)][8]+"','"+Data[random.nextInt(200)][9]+");";
+                System.out.println(insertSQL_UserInfo);
+                //nowState.execute(insertSQL_UserInfo);
+                System.out.println("insert "+i);
+                System.out.println("\n------------------------------------------");
+            }
 
 
         } catch(SQLException e) {
